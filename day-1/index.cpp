64f9b58d9f59d2b7fbf1d38509be3317574b88dd
sonugup <skg30336@gmail.com>
2024-04-11 17:49:46 +0530

    types of veriables

--- day-1/index.cpp
+++ day-1/index.cpp
@@ -3,4 +3,16 @@ using namespace std;
 
 int main() {
     cout << "Namaste Universe" <<endl;
+    char a='A';
+    cout << a << endl;
+    int b=9;
+    cout << b << endl;
+    bool bl=true;
+    cout << bl << endl;
+    float f=1.2;
+    cout << f << endl;
+    double d=1.234;
+    cout << d << endl;
+    int s=sizeof(d);
+    cout << s<<endl;
 }
