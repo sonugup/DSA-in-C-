fb0180285b33c49b9e4f5a052c28c669d752128b
sonugup <skg30336@gmail.com>
2024-06-20 19:07:44 +0530

    signed cyclic

--- c-prog/day-1.c
+++ c-prog/day-1.c
@@ -4,7 +4,10 @@ void main(){
     char x=128;
     printf("%c", x);
 
-    char x1=128;
-    printf("%d", x);
+    char x1=129;
+    // printf("%d", x1); //o/p = -127
+
+    signed short int a=65539;
+    printf("%d", a); // o/p=3
 }
 
